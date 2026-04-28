d03317b36df7831c6f91c25fd9535dbe623d5e74
Samone Cook <samone.a.cook-1@ou.edu>
2026-04-28 23:52:42 +0000

    deleted main.py file and updated evaluator.py to work with gemini

--- circuit-debug-ai/evaluator.py
+++ circuit-debug-ai/evaluator.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash")
+
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 def verify(problem, diagnosis):
     prompt = f"""
